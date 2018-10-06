491c0a1355b0670cf4bda4795762fb3843fb9f64
MarcusEstr <estrada.marcus@gmail.com>
2018-10-05 21:57:15 -0700

    Added non-async related test suites.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,59 +1,63 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
-
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
+//Don't run tests until DOM is ready. 
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+    /* This suite is all about RSS feeds
+    * definitions/the allFeeds variable in our application. */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+        /* Look at alLFeeds array:
+         * Expects allFeeds has been defined.
+         * Expects that allFeeds is not empty. */
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        /* Look at each feed in allFeeds array:
+         * Expects it has a URL defined.
+         * Expects that the URL is not empty.*/
+         it('url defined', function() {
+            for(let feed of allFeeds) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url).not.toBeFalsy();
+            }
+         });
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
+        /* Look at each feed in allFeeds array:
+         * Expects it has a name defined.
+         * Expects that the name is not empty.*/
+         it('name defined', function() {
+            for(let feed of allFeeds) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBeFalsy();
+            }
+         });
+
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    // Test suite for the expanding/collapsing menu.
     describe('The menu', function() {
+        //Variables body and menu to be used by tests:
+        const body = document.querySelector('body');
+        const menu = document.querySelector('.menu-icon-link');
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        /* Look at menu:
+         * Expects menu element is hidden by default. */
+         it('is hidden by default', function() {
+            expect(body.classList.contains('menu-hidden')).toBe(true);
+         });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+         /* Menu changes visibility when the menu icon is clicked:
+          * Expects menu display when clicked.
+          * Expects menu hides when clicked again. */
+          it('changes visibility', function() {
+            menu.click(); // Simulate click on menu icon.
+            expect(body.classList.contains('menu-hidden')).toBe(false);
+            menu.click();
+            expect(body.classList.contains('menu-hidden')).toBe(true);
+          });
+
     });
 
 
